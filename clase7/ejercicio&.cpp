ccec4887505a5913ae0a012f8d31cf938e99c2a0
Marissa Chamba <marissa.chamba@gmail.com>
2026-09-07 20:53:50 -0500

    crear archivo de & y modulalizacion

---
+++ clase7/ejercicio&.cpp
@@ -0,0 +1,25 @@
+/*
+REPASO 
+
+SIN & = COPIA
+CON & VARIALE ORIGINAL
+
+*/
+
+#include <iostream>
+using namespace std;
+void aumentar(int x){
+    x = x + 1;
+    cout <<"Dentro de la funcion, x vale: " << x << endl;
+
+}
+
+int main(){
+    int a = 10;
+
+    aumentar(a);
+
+    cout <<"Fuera de la funcion, a vale: " << a << endl;
+
+    return 0;
+}
