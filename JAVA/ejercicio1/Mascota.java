70503e4faaf838f3dde17e34dd917a66a8310f7d
Marissa Chamba <marissa.chamba@gmail.com>
2026-09-22 17:10:01 -0500

    tarea finalizada

--- JAVA/ejercicio1/Mascota.java
+++ JAVA/ejercicio1/Mascota.java
@@ -1,12 +1,18 @@
 public class mascota {
      String nombre;
-    String especie;
-    int edad;        
+     String especie;
+     int edad;   
+    
+    public Mascota(string nombre, string especie, int edad){
+     this.nombre = nombre;
+     this.especie = especie;
+     this.edad  = edad;
+    } 
+
+    public void mostrarInfo(){
+     System.out.println("Nombre: " + nombre);
+     System.out.println("Especie : " + especie);
+     System.out.println("Edad: " + edad + " años");
+    }
 }
 
-//constructor
-public Mascota(string nombre, string especie, int edad){
-    this.nombre = nombre;
-    this.especie = especie;
-    this.edad  = edad;
-} 
--- JAVA/ejercicio1/src/App.java
+++ JAVA/ejercicio1/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Mascota mascota1 = new Mascota("Bruno". "perro", 3);
+        Mascota mascota2 = new Mascota("Michi", "Gato", 2);
+
+        System.out.println("==Mascota 1 ==");
+        mascota1.mostrarInfo();
+
+        System.out.println("==Mascota 2 ==");
+        mascota2.mostrarInfo();
     }
 }
