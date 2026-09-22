1384c486ac5792f6d1691a6857235a1e1f75c804
Marissa Chamba <marissa.chamba@gmail.com>
2026-09-22 17:50:55 -0500

    creacion del constructor

--- JAVA/ejercicio2/src/telefono.java
+++ JAVA/ejercicio2/src/telefono.java
@@ -3,5 +3,17 @@ public class telefono {
     private String marca;
     private String modelo;
     private double precio;
+
+    public Telefono(String marca, String modelo, double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        
+        if (precio >= 0) {
+            this.precio = precio;
+        } else {
+            System.out.println("El precio no puede ser negativo. Se asignó 0.0");
+            this.precio = 0.0;
+        }
+    }
     
 }
