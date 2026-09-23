29b9ab532942e2da79f594e953dfea15d4ee9263
Marissa Chamba <marissa.chamba@gmail.com>
2026-09-22 19:33:05 -0500

    creacion de setters

--- JAVA/ejercicio2/src/telefono.java
+++ JAVA/ejercicio2/src/telefono.java
@@ -27,5 +27,24 @@ public class telefono {
       return precio;
     }
 
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setPrecio(double precio) {
+        
+        if (precio >= 0) {
+            this.precio = precio;
+        } else {
+            System.out.println("Precio inválido. No se puede asignar un valor negativo.");
+        }
+    }
+
+
     
 }
